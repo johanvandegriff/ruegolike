5d3f3990350b5113183e9dbedaa5ab7c094ed7ee
Johan Vandegriff <johan@vandymail.com>
2020-05-15 12:55:56 -0400

    more raycasting tests

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"math/rand"
 	"os"
 	"time"
@@ -59,6 +60,73 @@ func Abs8(x int8) int8 {
 }
 
 //https://playtechs.blogspot.com/2007/03/raytracing-on-grid.html
+func traceLine(x0, y0, x1, y1 float64) [][]int {
+	// x0 += 0.5
+	// y0 += 0.5
+	// x1 += 0.5
+	// y1 += 0.5
+	// x0 := float64(x0i) + 0.5
+	// y0 := float64(y0i) + 0.5
+	// x1 := float64(x1i) + 0.5
+	// y1 := float64(y1i) + 0.5
+
+	dx := math.Abs(x1 - x0)
+	dy := math.Abs(y1 - y0)
+
+	x := int(math.Floor(x0))
+	y := int(math.Floor(y0))
+
+	n := 1
+	var xInc, yInc int
+	var error float64
+
+	if dx == 0 {
+		xInc = 0
+		error = math.Inf(1)
+	} else if x1 > x0 {
+		xInc = 1
+		n += int(math.Floor(x1)) - x
+		error = (math.Floor(x0) + 1 - x0) * dy
+	} else {
+		xInc = -1
+		n += x - int(math.Floor(x1))
+		error = (x0 - math.Floor(x0)) * dy
+	}
+
+	if dy == 0 {
+		yInc = 0
+		error -= math.Inf(1)
+	} else if y1 > y0 {
+		yInc = 1
+		n += int(math.Floor(y1)) - y
+		error -= (math.Floor(y0) + 1 - y0) * dx
+	} else {
+		yInc = -1
+		n += y - int(math.Floor(y1))
+		error -= (y0 - math.Floor(y0)) * dx
+	}
+
+	points := make([][]int, n)
+
+	i := 0
+	for ; n > 0; n-- {
+		// fmt.Println(x, y, error)
+		points[i] = make([]int, 2)
+		points[i][0] = x
+		points[i][1] = y
+		i++
+
+		if error > 0 {
+			y += yInc
+			error -= dx
+		} else {
+			x += xInc
+			error += dy
+		}
+	}
+	return points
+}
+
 func traceLineInt(x0, y0, x1, y1 int) [][]int {
 	dx := Abs(x1 - x0)
 	dy := Abs(y1 - y0)
@@ -79,11 +147,24 @@ func traceLineInt(x0, y0, x1, y1 int) [][]int {
 
 	points := make([][]int, n)
 	i := 0
+	// changed := false
 	for ; n > 0; n-- {
 		// fmt.Println(x, y, error)
 		points[i] = make([]int, 2)
 		points[i][0] = x
 		points[i][1] = y
+		// if i >= 2 && points[i-2][0] == points[i-1][0] && points[i-1][0] != points[i][0] {
+		// 	if !changed {
+		// 		points[i][0] = points[i-1][0]
+		// 	}
+		// 	changed = !changed
+		// }
+		// if i >= 2 && points[i-2][1] == points[i-1][1] && points[i-1][1] != points[i][1] {
+		// 	if !changed {
+		// 		points[i][1] = points[i-1][1]
+		// 	}
+		// 	changed = !changed
+		// }
 		i++
 
 		if error > 0 {
@@ -123,6 +204,7 @@ func findNeighbors(x, y int, level [width][height]int32) int {
 }
 
 func isSquareObstructing(x0, y0, x1, y1, x, y int, level [width][height]int32) bool {
+	// return true //TODO tmp
 	//pre-checks
 	if y1 < y0 {
 		x0, y0, x1, y1 = x1, y1, x0, y0 //make sure that y1 >= y0
@@ -138,12 +220,15 @@ func isSquareObstructing(x0, y0, x1, y1, x, y int, level [width][height]int32) b
 	if y0 == y1 {
 		return y0 == y
 	}
-	m := float64(y1-y0) / float64(x1-x0)
-	b := float64(y0) - m*float64(x0)
-	if m == 1 || m == -1 {
-		return y == int(m*float64(x)+b)
+	// if m == 1 || m == -1 {
+	if (y1 - y0) == (x1 - x0) {
+		return x-x0 == y-y0
+	}
+	if (y1 - y0) == (x0 - x1) {
+		return x-x0 == y0-y
 	}
 	//now, it is guaranteed that x0 != x1, y0 != y1, y1 > y0, m != 1, m != -1
+	// return true //TODO tmp
 
 	neighborsToLines := [16][4]uint8{
 		//A B  C  D
@@ -164,10 +249,24 @@ func isSquareObstructing(x0, y0, x1, y1, x, y int, level [width][height]int32) b
 		{1, 2, 0, 0},
 		{0, 0, 0, 0}}
 	lines := neighborsToLines[findNeighbors(x, y, level)]
-	// fmt.Print(lines, " ")
-	xf := float64(x)
-	yf := float64(y)
-	// fmt.Print("(", x, y, ")")
+	xf, yf := float64(x), float64(y)
+	m := float64(y1) - yf/float64(x1) - xf
+	b := yf - m*xf
+	return doLinesIntersect(xf, yf, m, b, lines)
+
+	// for xf := float64(x0) - 0.5; xf <= float64(x0)+0.5; xf += 0.5 {
+	// 	for yf := float64(y0) - 0.5; yf <= float64(y0)+0.5; yf += 0.5 {
+	// 		m := float64(y1) - yf/float64(x1) - xf
+	// 		b := yf - m*xf
+	// 		if !doLinesIntersect(xf, yf, m, b, lines) {
+	// 			return false
+	// 		}
+	// 	}
+	// }
+	// return true
+}
+
+func doLinesIntersect(xf, yf, m, b float64, lines [4]uint8) bool {
 	if lines[0] == 1 { //A1
 		yi := (m*(-yf+.5+xf) + b) / (1 - m)
 		if yf-.5 <= yi && yi <= yf {
@@ -238,8 +337,36 @@ func isSquareObstructing(x0, y0, x1, y1, x, y int, level [width][height]int32) b
 
 }
 
-func canPlayerSee(playerX, playerY, x, y int, level [width][height]int32) bool {
+/*func canPlayerSee(playerX, playerY, x, y int, level [width][height]int32) bool {
+	canSee := false
+	for startX := float64(x) - 0.5; startX <= float64(x)+0.5; startX += 0.5 {
+		for startY := float64(y) - 0.5; startY <= float64(y)+0.5; startY += 0.5 {
+			isRayBlocked := false
+			points := traceLine(startX, startY, float64(playerX), float64(playerY))
 
+			for index, point := range points {
+				px := point[0]
+				py := point[1]
+				if index != 0 && index != len(points)-1 &&
+					isXYInRange(px, py) && level[px][py] == '#' &&
+					isSquareObstructing(playerX, playerY, x, y, px, py, level) {
+					isRayBlocked = true
+					break
+				}
+			}
+			if !isRayBlocked {
+				canSee = true
+				break
+			}
+		}
+		if canSee {
+			break
+		}
+	}
+	return canSee
+}*/
+
+func canPlayerSee(playerX, playerY, x, y int, level [width][height]int32) bool {
 	points := traceLineInt(playerX, playerY, x, y)
 
 	for index, point := range points {
@@ -258,9 +385,13 @@ func raycast(playerX int, playerY int, visible [width][height]bool, explored [wi
 	//calculate visible and explored tiles with raycasting
 	for x := 0; x < width; x++ {
 		for y := 0; y < height; y++ {
-			visible[x][y] = canPlayerSee(playerX, playerY, x, y, level)
-			if visible[x][y] {
-				explored[x][y] = true
+			if playerX-5 <= x && x <= playerX+5 && playerY-5 <= y && y <= playerY+5 {
+				visible[x][y] = canPlayerSee(playerX, playerY, x, y, level)
+				if visible[x][y] {
+					explored[x][y] = true
+				}
+			} else {
+				visible[x][y] = false
 			}
 		}
 	}
@@ -287,6 +418,7 @@ func main() {
 	// style2 := tcell.StyleDefault.Foreground(tcell.ColorDarkGray).Background(tcell.ColorBlack)
 	style := tcell.StyleDefault.Foreground(tcell.ColorWhite).Background(tcell.ColorRed)
 	style2 := tcell.StyleDefault.Foreground(tcell.ColorDarkGray).Background(tcell.ColorBlack)
+	style3 := tcell.StyleDefault.Foreground(tcell.ColorWhite).Background(tcell.ColorDarkGray)
 
 	// invert := tcell.StyleDefault.Foreground(tcell.ColorBlack).Background(tcell.ColorWhite)
 
@@ -388,7 +520,7 @@ func main() {
 					}
 				} else {
 					// s.SetContent(x+offsetX, y+offsetY, ' ', nil, style)
-					s.SetContent(x+offsetX, y+offsetY, level[x][y], nil, style2) //tmp
+					s.SetContent(x+offsetX, y+offsetY, level[x][y], nil, style3) //tmp
 				}
 			}
 		}
